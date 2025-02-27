7eea2cd02e6b3db22f8d19e55a21c110b3b492d8
embaabu <edwin.mbaabu@techconsulting.tech>
2025-02-27 09:02:48 -0500

    Initial project

---
+++ DigimonApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DigimonApp
+//
+//  Created by Edwin Mbaabu on 2/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DigimonApp/DigimonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigimonAppApp.swift
+//  DigimonApp
+//
+//  Created by Edwin Mbaabu on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DigimonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
